f4aec9cee660520bd9aa58e4ed2947355f0c7fee
InprisLtd <developer@inprisltd.com>
2017-07-22 13:35:33 +0300

    Initial commit project

---
+++ app/src/main/java/itzik/com/simulationscreen/SimulationScreen.java
@@ -0,0 +1,13 @@
+package itzik.com.simulationscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimulationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulation_screen);
+    }
+}
